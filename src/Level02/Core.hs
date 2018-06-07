c745151e205f983169effeda32c539dc8660390d
Chris Davenport <chris@christopherdavenport.tech>
2018-06-07 17:25:31 -0400

    Finish Level 02

--- src/Level02/Core.hs
+++ src/Level02/Core.hs
@@ -16,7 +16,7 @@ import           Data.Either              (either)
 import           Data.Text                (Text)
 import           Data.Text.Encoding       (decodeUtf8)
 
-import           Level02.Types           (ContentType, Error, RqType,
+import           Level02.Types           (ContentType(..), Error(..), RqType(..),
                                            mkCommentText, mkTopic,
                                            renderContentType)
 
@@ -25,43 +25,25 @@ import           Level02.Types           (ContentType, Error, RqType,
 -- --------------------------------------------
 
 -- | Some helper functions to make our lives a little more DRY.
-mkResponse
-  :: Status
-  -> ContentType
-  -> LBS.ByteString
-  -> Response
-mkResponse =
-  error "mkResponse not implemented"
+mkResponse :: Status -> ContentType -> LBS.ByteString -> Response
+mkResponse s ct bs = responseLBS s [("content-type" , (renderContentType ct))] bs
 
-resp200
-  :: ContentType
-  -> LBS.ByteString
-  -> Response
-resp200 =
-  error "resp200 not implemented"
+resp200 :: ContentType -> LBS.ByteString -> Response
+resp200 ct bs = responseLBS status200 [("content-type", (renderContentType ct))] bs
 
-resp404
-  :: ContentType
-  -> LBS.ByteString
-  -> Response
-resp404 =
-  error "resp404 not implemented"
+resp404 :: ContentType -> LBS.ByteString -> Response
+resp404 ct bs = responseLBS status404 [("content-type", (renderContentType ct))] bs
 
-resp400
-  :: ContentType
-  -> LBS.ByteString
-  -> Response
-resp400 =
-  error "resp400 not implemented"
+resp400 :: ContentType -> LBS.ByteString -> Response
+resp400 ct bs = responseLBS status400 [("content-type", (renderContentType ct))] bs
 
 -- These next few functions will take raw request information and construct one
 -- of our types.
-mkAddRequest
-  :: Text
-  -> LBS.ByteString
-  -> Either Error RqType
-mkAddRequest =
-  error "mkAddRequest not implemented"
+mkAddRequest :: Text -> LBS.ByteString -> Either Error RqType
+mkAddRequest t bs = do
+  comment <- (mkCommentText . lazyByteStringToStrictText) bs
+  topic <- mkTopic t
+  return $ AddRq topic comment
   where
     -- This is a helper function to assist us in going from a Lazy ByteString, to a Strict Text
     lazyByteStringToStrictText =
@@ -71,33 +53,30 @@ mkAddRequest =
 -- requirements into smaller components that are simpler to maintain and verify.
 -- It also allows for greater reuse and it also means that validation is not
 -- duplicated across the application, maybe incorrectly.
-mkViewRequest
-  :: Text
-  -> Either Error RqType
-mkViewRequest =
-  error "mkViewRequest not implemented"
+mkViewRequest :: Text -> Either Error RqType
+mkViewRequest t = fmap ViewRq (mkTopic t)
 
-mkListRequest
-  :: Either Error RqType
-mkListRequest =
-  error "mkListRequest not implemented"
+mkListRequest :: Either Error RqType
+mkListRequest = Right ListRq
 
-mkErrorResponse
-  :: Error
-  -> Response
-mkErrorResponse =
-  error "mkErrorResponse not implemented"
+--  EmptyTopic | EmptyCommentText | MissingTopic
+mkErrorResponse :: Error -> Response
+mkErrorResponse EmptyTopic        = resp404 PlainText "EmptyTpic"
+mkErrorResponse EmptyCommentText  = resp404 PlainText "EmptyCommentText"
+mkErrorResponse MissingTopic      = resp400 PlainText "Missing Topic"
+mkErrorResponse UnmatchedRequest  = resp400 PlainText "Unmatched Request"
 
 -- Use our ``RqType`` helpers to write a function that will take the input
 -- ``Request`` from the Wai library and turn it into something our application
 -- cares about.
-mkRequest
-  :: Request
-  -> IO ( Either Error RqType )
-mkRequest =
-  -- Remembering your pattern-matching skills will let you implement the entire
-  -- specification in this function.
-  error "mkRequest not implemented"
+mkRequest :: Request -> IO ( Either Error RqType )
+mkRequest request = case ((requestMethod request), (pathInfo request)) of
+  ("POST", topic:["add"]) -> 
+    fmap (\body -> mkAddRequest topic body) (strictRequestBody request) 
+  ("GET", topic:["view"]) -> pure $ mkViewRequest topic
+  ("GET", ["list"]) -> pure mkListRequest
+  _ -> pure $ Left UnmatchedRequest
+  
 
 -- If we find that we need more information to handle a request, or we have a
 -- new type of request that we'd like to handle then we update the ``RqType``
@@ -110,18 +89,18 @@ mkRequest =
 -- For now, return a made-up value for each of the responses as we don't have
 -- any persistent storage. Plain text responses that contain "X not implemented
 -- yet" should be sufficient.
-handleRequest
-  :: RqType
-  -> Either Error Response
-handleRequest =
-  error "handleRequest not implemented"
+handleRequest :: RqType -> Either Error Response
+handleRequest (AddRq t ct)  = Right $ resp200 PlainText "AddRq not implemented yet"
+handleRequest (ViewRq t)    = Right $ resp200 PlainText "ViewRq not implemented yet"
+handleRequest ListRq        = Right $ resp200 PlainText "ListRq not implemented yet"
+
 
 -- Reimplement this function using the new functions and ``RqType`` constructors
 -- as a guide.
-app
-  :: Application
-app =
-  error "app not reimplemented"
+app :: Application
+app req cb = do
+  rqType <- mkRequest req
+  cb $ either mkErrorResponse (\rqType -> either mkErrorResponse (\a -> a) (handleRequest rqType)) rqType
 
 runApp :: IO ()
 runApp = run 3000 app
--- src/Level02/Types.hs
+++ src/Level02/Types.hs
@@ -56,14 +56,14 @@ newtype CommentText = CommentText Text
 -- AddRq : Which needs the target topic, and the body of the comment.
 -- ViewRq : Which needs the topic being requested.
 -- ListRq : Which doesn't need anything and lists all of the current topics.
-data RqType
+data RqType = AddRq Topic CommentText | ViewRq Topic | ListRq
 
 -- Not everything goes according to plan, but it's important that our types
 -- reflect when errors can be introduced into our program. Additionally it's
 -- useful to be able to be descriptive about what went wrong.
 
 -- Fill in the error constructors as you need them.
-data Error
+data Error = EmptyTopic | EmptyCommentText | MissingTopic | UnmatchedRequest
 
 
 -- Provide the constructors for a sum type to specify the `ContentType` Header,
@@ -72,7 +72,7 @@ data Error
 --
 -- - plain text
 -- - json
-data ContentType
+data ContentType = PlainText | Json
 
 -- The ``ContentType`` constructors don't match what is required for the header
 -- information. Because ``wai`` uses a stringly type. So write a function that
@@ -85,11 +85,9 @@ data ContentType
 -- - plain text = "text/plain"
 -- - json       = "application/json"
 --
-renderContentType
-  :: ContentType
-  -> ByteString
-renderContentType =
-  error "renderContentType not implemented"
+renderContentType :: ContentType -> ByteString
+renderContentType PlainText = "text/plain"
+renderContentType Json = "application/json"
 
 -- We can choose to *not* export the constructor for a data type and instead
 -- provide a function of our own. In our case, we're not interested in empty
@@ -99,28 +97,18 @@ renderContentType =
 -- The export list at the top of this file demonstrates how to export a type,
 -- but not export the constructor.
 
-mkTopic
-  :: Text
-  -> Either Error Topic
-mkTopic =
-  error "mkTopic not implemented"
+mkTopic :: Text -> Either Error Topic
+mkTopic ""    = Left EmptyTopic
+mkTopic text  = Right (Topic text)
 
-getTopic
-  :: Topic
-  -> Text
-getTopic =
-  error "getTopic not implemented"
+getTopic :: Topic -> Text
+getTopic (Topic a) = a
 
-mkCommentText
-  :: Text
-  -> Either Error CommentText
-mkCommentText =
-  error "mkCommentText not implemented"
+mkCommentText :: Text -> Either Error CommentText
+mkCommentText "" =  Left EmptyCommentText
+mkCommentText text = Right (CommentText text)
 
-getCommentText
-  :: CommentText
-  -> Text
-getCommentText =
-  error "getCommentText not implemented"
+getCommentText :: CommentText -> Text
+getCommentText (CommentText text) = text
 
 ---- Go to `src/Level02/Core.hs` next
